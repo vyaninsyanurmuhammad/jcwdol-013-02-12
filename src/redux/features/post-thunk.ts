4d86d7052afce9b5a7b27fad4f1bafb46428b80d
vyaninsyanurmuhammad <vyaninsya.nurmuhammad@gmail.com>
2024-03-14 13:57:24 +0700

    refactor (api): change to env.local

--- src/redux/features/post-thunk.ts
+++ src/redux/features/post-thunk.ts
@@ -5,7 +5,7 @@ import axios from "axios";
 
 export const getPostsThunk = createAsyncThunk("post/getPosts", async () => {
   const response = await axios.get(
-    "https://cdn.contentful.com/spaces/uflk394shvqp/environments/master/entries?access_token=mcTJDCh6ZHRl9asibu1Sl1ri6ehT3Zs4cu5sYyivQio&content_type=post"
+    `https://cdn.contentful.com/spaces/${process.env.CONTENTFUL_SPACE_ID}/environments/master/entries?access_token=${process.env.CONTENTFUL_ACCESS_TOKEN}&content_type=post`
   );
 
   const data: PostType = response.data;
@@ -13,7 +13,6 @@ export const getPostsThunk = createAsyncThunk("post/getPosts", async () => {
   const assetsDataCollections = data.includes.Asset;
 
   const result: IPost[] = data.items.map((item) => {
-
     const imageAssets = item.fields.image
       ? assetsDataCollections.filter(
           (asset) => item.fields.image?.sys.id === asset.sys.id
@@ -23,13 +22,14 @@ export const getPostsThunk = createAsyncThunk("post/getPosts", async () => {
     return {
       id: item.sys.id,
       message: item.fields.message,
-      image: item.fields.image && imageAssets
-        ? {
-            id: item.fields.image?.sys.id,
-            title: imageAssets[0].fields.title,
-            url: imageAssets[0].fields.file.url,
-          }
-        : undefined,
+      image:
+        item.fields.image && imageAssets
+          ? {
+              id: item.fields.image?.sys.id,
+              title: imageAssets[0].fields.title,
+              url: imageAssets[0].fields.file.url,
+            }
+          : undefined,
     };
   });
 
